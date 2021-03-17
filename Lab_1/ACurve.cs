df28bd2b8eb39de01a71d4736bc64e0004912f87
Alexsey Soluyanov <al.soluyanov@rambler.ru>
2021-03-17 22:37:13 +0300

    lab_1: add geometry layer

---
+++ Lab_1/ACurve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    abstract class ACurve : ICurve
+    {
+        protected IPoint a;
+        protected IPoint b;
+
+        public ACurve(IPoint a_, IPoint b_)
+        {
+            this.a = a_;
+            this.b = b_;
+        }
+
+        public abstract IPoint GetPoint(double t);
+    }
+}
